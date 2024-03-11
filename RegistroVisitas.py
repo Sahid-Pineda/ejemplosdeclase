17c711b3986e6831b4c104ca741cd671ae861a4b
Sahid Pineda <nelsonsahid504@gmail.com>
2024-03-11 15:04:51 -0600

    Update

--- RegistroVisitas.py
+++ RegistroVisitas.py
@@ -52,6 +52,9 @@ class Persona:
     def getDireccion(self):
         return self.__direccion
     
+    def __str__(self):
+        return f"\nIdentidad: {self.getId()}\nNombre Completo: {self.getNombre1()} {self.getNombre2()} {self.getApellido1()} {self.getApellido2()}\nDirección: {self.getDireccion()}"
+#persona = Persona(123,"Sahid", "Nelson", "Pineda", "Gonzalez", "EZA")
 class Empleados(Persona):
     # - Numero de Empleado: int
 
@@ -66,6 +69,9 @@ class Empleados(Persona):
     #Getters
     def getNEmpleado(self):
         return self.__nEmpleado
+    
+    def __str__(self):
+        return super().__str__() + f"\nNo. Empleado: {self.getNEmpleado()}"
     
 class Maestro(Empleados):
     # - Facultad: string
@@ -90,11 +96,14 @@ class Maestro(Empleados):
     def getEspecializacion(self):
         return self.__especializacion
     
+    def __str__(self):
+        return super().__str__() + f"\nFacultad: {self.getFacultad()}\nEspecialización: {self.getEspecializacion()}\nInformación del Maestro"
+    
 class Administrativo(Empleados):
     # - Area: string
-    # - Campo: boolean
+    # - Campo: string
 
-    def __init__(self, id, nombre1, nombre2, apellido1, apellido2, direccion, nEmpleado, area, campo = False):
+    def __init__(self, id, nombre1, nombre2, apellido1, apellido2, direccion, nEmpleado, area, campo):
         super().__init__(id, nombre1, nombre2, apellido1, apellido2, direccion, nEmpleado)
         self.__area = area
         self.__campo = campo
@@ -112,6 +121,9 @@ class Administrativo(Empleados):
     
     def getCampo(self):
         return self.__campo
+    
+    def __str__(self):
+        return super().__str__() + f"Área: {self.getArea()}\nCampo: {self.getCampo()}\nInformación de Empleado Administrativo"
     
 class Alumno(Persona):
     # - Carrera: string
@@ -144,6 +156,9 @@ class Alumno(Persona):
     def getNCuenta(self):
         return self.__nCuenta
     
+    def __str__(self):
+        return super().__str__() + f"\nCarrera: {self.getCarrera()}\nHorario: {self.getHorario()}\nNo. de Cuenta: {self.getNCuenta()}\nInformación del Alumno"
+    
 class Visitas(Persona):
     # - Razon Visita: string
     # - Edificio: string
@@ -167,16 +182,19 @@ class Visitas(Persona):
     def getEdificio(self):
         return self.__edificio
     
+    def __str__(self):
+        return super().__str__() + f"\nRazón de Visita: {self.getRazonVisita()}\nEdificio: {self.getEdificio()}\nInformación del Visitante"
+    
 class Jefe(Maestro, Administrativo):
     # - Fecha de Inicio: string
     # - Fecha Fin: string
     # - Jefe: boolean
-
-    def __init__(self, id, nombre1, nombre2, apellido1, apellido2, direccion, nEmpleado, facultad, especializacion, fechaInicio, fechaFin, jefe = False):
-        super().__init__(id, nombre1, nombre2, apellido1, apellido2, direccion, nEmpleado, facultad, especializacion)
+    def __init__(self, id, nombre1, nombre2, apellido1, apellido2, direccion, nEmpleado, facultad, especializacion, fechaInicio, fechaFin, jefe):
+        Maestro.__init__(self, id, nombre1, nombre2, apellido1, apellido2, direccion, nEmpleado, facultad, especializacion)
+        Administrativo.__init__(self, id, nombre1, nombre2, apellido1, apellido2, direccion, nEmpleado, None, None)
         self.__fechaInicio = fechaInicio
         self.__fechaFin = fechaFin
-        self.__jefe = jefe
+        self.__jefe = bool(jefe)
 
     #Setters
     def setFechaInicio(self, fechaInicio):
@@ -197,4 +215,95 @@ class Jefe(Maestro, Administrativo):
     
     def getJefe(self):
         return self.__jefe
+    
+    def __str__(self):
+        return super().__str__() + f"Fecha de Inicio: {self.getFechaInicio()}\nFecha Fin: {self.getFechaFin()}\nJefe: {self.getJefe()}\nInformación del Jefe/Coordinador"
+    
+class RegistroIngreso:
+    def __init__(self):
+        self.__visitas = []
 
+    def agregarVisita(self, tipo):
+        print("\n\tREGISTRANDO INFORMACIÓN")
+        id = int(input("Ingrese su número de Identidad: "))
+        nombre1 = input("Ingrese su primer nombre: ")
+        nombre2 = input("Ingrese su segundo nombre: ")
+        apellido1 = input("Ingrese su primer apellido: ")
+        apellido2 = input("Ingrese su segundo apellido: ")
+        direccion = input("Ingrese su dirección: ")
+
+        if tipo == 1: # Agregando Empleado
+            nEmpleado = int(input("Ingrese su número de Empleado: "))
+            rol = int(input("\n1. Si es Maestro\n2. Si es Administrativo\n3. Si es Jefe\nSeleccione una opción: "))
+            if rol == 1:
+                facultad = input("\nIngrese su Facultad: ")
+                especializacion = input("Ingrese su Especialización: ")
+                nuevo_maestro = Maestro(id, nombre1, nombre2, apellido1, apellido2, direccion, nEmpleado, facultad, especializacion)
+                self.__visitas.append(nuevo_maestro)
+            elif rol == 2:
+                area = input("\nIngrese su área: ")
+                campo = input("Ingrese su campo: ")
+                nuevo_administrativo = Administrativo(id, nombre1, nombre2, apellido1, apellido2, direccion, nEmpleado, area, campo)
+                self.__visitas.append(nuevo_administrativo)
+            elif rol == 3:
+                facultad = input("\nIngrese su Facultad: ")
+                especializacion = input("Ingrese su Especialización: ")
+                # areaBoss = input("Ingrese su área: ")
+                # campoBoss = input("Ingrese su campo: ")
+                fechaInicio = input("Ingrese su Fecha de inicio en el cargo: ")
+                fechaFin = input("Ingrese su Fecha Fin en el cargo: ")
+                identificarCargo = input("Si es Jefe escriba 'j', si es Coordinador escriba 'c': ")
+                jefe = identificarCargo.lower() == 'j'
+                nuevo_jefe = Jefe(id, nombre1, nombre2, apellido1, apellido2, direccion, nEmpleado, facultad, especializacion, fechaInicio, fechaFin, jefe)
+                self.__visitas.append(nuevo_jefe)
+        elif tipo == 2: #Agregando Alumno
+            carrera = input("Ingrese el nombre de la carrera: ")
+            horario = input("Ingrese horario: ")
+            nCuenta = input("Ingrese su No. de Cuenta: ")
+            nuevo_alumno = Alumno(id, nombre1, nombre2, apellido1, apellido2, direccion, carrera, horario, nCuenta)
+            self.__visitas.append(nuevo_alumno)
+        elif tipo == 3: #Agregando Visita
+            razonVisita = input("Ingrese razón de visita: ")
+            edificio = input("Ingrese el edificio al que se dirige: ")
+            nueva_visita = Visitas(id, nombre1, nombre2, apellido1, apellido2, direccion, razonVisita, edificio)
+            self.__visitas.append(nueva_visita)
+        else:
+            print("Opcion no válida.")
+        print("Registrado correctamente.")
+
+    def mostrarVisitas(self):
+        print("\nINFORMACION DEL REGISTRO DE INGRESO")
+        for visita in self.__visitas:
+            print(visita)
+
+visitas = RegistroIngreso()
+
+seguir = True
+
+while(seguir):
+    try:
+        print("\n------ Menu ------")
+        print("1. Registrar Visitante")
+        print("2. Mostrar Visitantes")
+        print("3. Actualizar Información Visitantes")
+        print("4. Eliminar Visitante")
+        print("5. Salir")
+        opcion = int(input("Seleccione una opción: "))
+    except:
+        print("Opcion no valida")
+    else:
+        match opcion:
+            case 1:
+                tipo = int(input("\n1. Empleado\n2. Alumno\n3. Visitante\nSeleccione una opción: "))
+                visitas.agregarVisita(tipo)
+            case 2:
+                visitas.mostrarVisitas()
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                print("Saliendo...")
+                seguir = False
+            case default:
+                print("Opción no válida")
